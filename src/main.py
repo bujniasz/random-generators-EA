22ca39c9a75a9882035e4d443552b60971548d02
abujnow1 <01161685@pw.edu.pl>
2025-05-22 21:13:35 +0200

    add stats analysis

--- src/main.py
+++ src/main.py
@@ -4,42 +4,80 @@ from evolutionary_alg import evolutionary_classic
 from rng_factory import RNG
 import time
 import secrets
+import pandas as pd
+import os
+import subprocess
+import sys
+import shutil
+
+for folder in ["plots", "results_data"]:
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
 
 if __name__ == "__main__":
-    MAX_X = 100  # Boundary limit for the solution
+    MAX_X = 100
     DIMENSIONALITY = 10
-
-    RUNS = 3        # Number of runs for the optimization algorithm
-    U = 10          # Population size
-    FES = 50000     # Number of objective function evaluationsW
+    RUNS = 3
+    U = 20
+    FES = 50000
     PC = 0.5
     DELTA_S = 0.1
     DELTA_B = 10
     P_BIG_JUMP = 0.02
 
-    res = []
+    FUNCTIONS = {
+        "f2": f2,
+        "f13": f13
+    }
 
-    for i in range(RUNS):
-        p0 = []
-        #seed = int(time.time() * 1e6) % 2**32
-        seed = secrets.randbits(32)
-        print(seed)
-        rng = RNG("random", DIMENSIONALITY, seed=seed)      #Mersenne Twister
-        #rng = RNG("numpy", DIMENSIONALITY, seed=seed)      #PCG
-        #rng = RNG("xoshiro", DIMENSIONALITY, seed=seed)    #xoshiro256
-        #rng = RNG("sobol", DIMENSIONALITY, seed=seed)      #Sobol   
-        #rng = RNG("halton", DIMENSIONALITY, seed=seed)     #Halton
-        #rng = RNG("lattice", DIMENSIONALITY, seed=seed)    #Lattice
+    GENERATORS = [
+        "random",   # Mersenne Twister
+        "numpy",    # PCG
+        "xoshiro",  # xoshi#ro256
+        "sobol",    # Sobol
+        "halton",   # Halton
+        "lattice"   # Lattice
+    ]
+
+    results = []
+
+    for func_name, func in FUNCTIONS.items():
+        for gen_name in GENERATORS:
+            print(f"\n=== {func_name} | {gen_name} ===")
+            for i in range(RUNS):
+                seed = secrets.randbits(32)
+                rng = RNG(gen_name, DIMENSIONALITY, seed=seed)
+
+                # Population initialization
+                p0 = [
+                    np.array(rng.uniform(-MAX_X, MAX_X, 1)).reshape(-1)
+                    for _ in range(U)
+                ]
+
+                t_max = FES / U
+
+                start_time = time.time()
+                score, _ = evolutionary_classic(
+                    func, p0, U, DELTA_S, DELTA_B,
+                    P_BIG_JUMP, PC, t_max, MAX_X, rng
+                )
+                run_time = time.time() - start_time
+
+                results.append({
+                    "function": func_name,
+                    "generator": gen_name,
+                    "score": f"{score:.2f}",
+                    "run_time": f"{run_time:.2f}",
+                })
 
 
-        for j in range(U):
-            x = np.array(rng.uniform(-MAX_X, MAX_X, 1)).reshape(-1)
-            p0.append(x)
+    os.makedirs("results_data", exist_ok=True)
 
-        t_max = FES / U  # Maximum number of generations (iterations)
-        o, x = evolutionary_classic(f2, p0, U, DELTA_S, DELTA_B, P_BIG_JUMP, PC, t_max, MAX_X, rng)
-        res.append(o)
+    df = pd.DataFrame(results)
+    df.to_csv("results_data/main_results.csv", index=False)
+    print("\n[OK] Zapisano wyniki do results_data/main_results.csv")
 
-    print(
-        f"min: {np.min(res):.3f}, max: {np.max(res):.3f}, avg: {np.mean(res):.3f}, std: {np.std(res):.3f}"
-    )
+    print("[INFO] Uruchamiam analizę statystyczną...")
+    subprocess.run([sys.executable, "src/stats_analysis.py"])
+
+    print("[OK] Analiza statystyczna zakończona")
